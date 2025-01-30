ba84e4cde229ba8b65f06d0b39603d20bf4ab2d1
jojobote <johann.biteghe@outlook.com>
2025-01-30 14:59:41 +0000

    fixing empty values

--- src/Resources/views/forms/fields/multiplechoice.php
+++ src/Resources/views/forms/fields/multiplechoice.php
@@ -1,8 +1,8 @@
 <select id="<?php echo $name; ?>" name="<?php echo $name; ?>[]" multiple class="snapSelect">
     <?php
     foreach ($choices as $choice) { ?>
-        <option value="<?php echo $choice ?>" 
-        <?php selected(in_array($choice, $currentValue)) ?> class="<?php echo $cssClass; ?>">
-        <?php echo $choice ?></option>
+        <option value="<?php echo $choice; ?>" 
+        <?php is_array($currentValue) ? selected(in_array($choice, $currentValue)) : '' ?> class="<?php echo $cssClass; ?>">
+        <?php echo $choice; ?></option>
     <?php } ?>
 </select>
