b95545b7f4a1fdf3bf47b27bbf97bb43a0af73c2
Clobee <cbindika@gmail.com>
2025-04-28 08:21:48 +0100

    Helps with tests breaking

--- tests/bootstrap.test.php
+++ tests/bootstrap.test.php
@@ -1,5 +1,5 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
-require_once GLOBAL_CFO_PLUGIN_FOLDER . '/cfo-info.php';
-require __DIR__.'/../autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+require_once  __DIR__ . '/../cfo-info.php';
+require __DIR__ . '/../autoload.php';
