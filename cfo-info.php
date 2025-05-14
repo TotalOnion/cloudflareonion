2aae3eceba15478a342f7ea21c6e219a194b532e
cl0b3e ☁️ <3452074+clobee@users.noreply.github.com>
2025-05-14 13:54:32 +0100

    Update cfo-info.php #major

--- cfo-info.php
+++ cfo-info.php
@@ -5,7 +5,6 @@ define('GLOBAL_CFO_NAMESPACE', 'GlobalCfo');
 define('GLOBAL_CFO_PLUGIN_FOLDER', __DIR__);
 
 
-
 if (function_exists('plugin_dir_url')) {
     define('GLOBAL_CFO_PLUGIN_URL', plugin_dir_url(__FILE__));
 }
