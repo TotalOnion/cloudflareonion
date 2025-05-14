b1372b67240e1508df480081eebbce4173636633
cl0b3e ☁️ <3452074+clobee@users.noreply.github.com>
2025-05-14 13:45:08 +0100

    Update cfo-info.php

--- cfo-info.php
+++ cfo-info.php
@@ -4,6 +4,8 @@ define('GLOBAL_CFO_NAME', 'global-cfo');
 define('GLOBAL_CFO_NAMESPACE', 'GlobalCfo');
 define('GLOBAL_CFO_PLUGIN_FOLDER', __DIR__);
 
+
+
 if (function_exists('plugin_dir_url')) {
     define('GLOBAL_CFO_PLUGIN_URL', plugin_dir_url(__FILE__));
 }
