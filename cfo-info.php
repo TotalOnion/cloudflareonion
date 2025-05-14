4dcb98a0784ebbec95197f2130b7df17e927660b
clobee <clobee@users.noreply.github.com>
2025-05-14 06:56:08 +0000

    (pipeline) Update project version: v0.1.0-beta.2
    Files changed:
    M	cfo-info.php

--- cfo-info.php
+++ cfo-info.php
@@ -1,5 +1,5 @@
 <?php
-define('GLOBAL_CFO_VERSION', '__GCFO_UNDEFINED_VERSION__');
+define('GLOBAL_CFO_VERSION', 'v0.1.0-beta.2');
 define('GLOBAL_CFO_NAME', 'global-cfo');
 define('GLOBAL_CFO_NAMESPACE', 'GlobalCfo');
 define('GLOBAL_CFO_PLUGIN_FOLDER', __DIR__);
